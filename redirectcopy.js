9c4189e9f645204c9f5846d5e15010c79b0402cc
quynhtrang1199 <quynhtrang1199@gmail.com>
2021-08-04 21:07:07 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://kenhtinbatdongsan.com.vn/batdongsan/sun-tropical-village-phu-quoc-biet-thu-nhiet-doi-bai-kem/'>https://kenhtinbatdongsan.com.vn/batdongsan/sun-tropical-village-phu-quoc-biet-thu-nhiet-doi-bai-kem/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://kenhtinbatdongsan.com.vn/'>https://kenhtinbatdongsan.com.vn/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
